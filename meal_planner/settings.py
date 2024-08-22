a1a34fb9390bd3a65643dc42b3121a2fe56aafca
Colin-CS50 <cayno334@gmail.com>
2024-08-22 17:54:28 +0900

    add logout and signup

--- meal_planner/settings.py
+++ meal_planner/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -131,4 +132,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
